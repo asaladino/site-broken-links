36688336cd3f5d57ee0fc97b339c7e75971e3822
Adam Saladino <adam@codingsimply.com>
2019-01-08 13:48:08 -0500

    Fixed bug where errors where not getting handled and crashed the scan.

--- src/Utility/IsLinkWorking.js
+++ src/Utility/IsLinkWorking.js
@@ -1,57 +1,11 @@
-'use strict';
-
-const WritableStream = require('stream').Writable;
 const got = require('got');
 
-class DevNull extends WritableStream {
-    _write(chunk, encoding, callback) {
-        callback();
-    }
-}
-
-function tryHead(link, gotOptions) {
-    return got.head(link, gotOptions)
-        .then(() => true);
-}
-
-function tryGet(link, options, gotOptions) {
-    return new Promise((resolve, reject) => {
-        let stream;
-        let req;
-
-        try {
-            stream = got.stream(link, gotOptions);
-        } catch (err) {
-            return resolve(false);
-        }
-
-        stream
-            .on('request', (req_) => {
-                req = req_;
-            })
-            .on('response', (res) => {
-                res.on('error', () => {
-                });  // Swallow any response errors, because we are going to abort the request
-                if (req) {
-                    setImmediate(() => req.abort());
-                }
-                resolve(true);
-            })
-            .on('error', (err, body, res) => {
-                return resolve(false);
-            })
-            .pipe(new DevNull());
-    });
-}
-
-// -------------------------------------------------------------------------
-
-function isLinkWorking(link, options) {
+async function isLinkWorking(link, options) {
     options = Object.assign({
         checkConnectivity: false,
         followRedirect: true,
         timeout: 15000,
-        retries: 3,
+        retries: 0,
         agent: null,
     }, options);
 
@@ -59,15 +13,19 @@ function isLinkWorking(link, options) {
         timeout: options.timeout,
         followRedirect: options.followRedirect,
         retries: options.retries,
+        throwHttpErrors: true,
         agent: options.agent,
         headers: {
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
         },
     };
 
-    return tryHead(link, gotOptions)
-        .catch(() => tryGet(link, options, gotOptions));
+    try {
+        await got(link, gotOptions);
+        return true;
+    } catch (error) {
+        return false;
+    }
 }
 
 module.exports = isLinkWorking;
-module.exports.connectivityCacheDuration = 5000;
